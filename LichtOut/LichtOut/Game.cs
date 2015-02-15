1c25ad3c8fbb2b7976f8fa019de6df6fba86944b
dentia <dyct.ent@gmail.com>
2015-02-15 11:49:48 +0200

    go guys!

---
+++ LichtOut/LichtOut/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LichtOut
+{
+    class Game
+    {
+        //create matrix of tiles
+        //print buttons depending on mode
+        //method to check if all lights are off
+    }
+}
